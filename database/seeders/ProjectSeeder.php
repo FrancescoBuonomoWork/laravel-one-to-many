41cefa4e96d7bf5b7514f31a818881f2b64f9059
FrancescoBuonomo <buonomofrancescow@gmail.com>
2024-01-09 16:29:37 +0100

    add type in projectSeeder

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -6,6 +6,7 @@ use App\Models\Project;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Faker\Generator as Faker;
+use App\Models\Type;
 
 class ProjectSeeder extends Seeder
 {
@@ -14,9 +15,13 @@ class ProjectSeeder extends Seeder
      */
     public function run(Faker $faker): void
     {
+        $types = Type::all();
+        $ids = $types->pluck("id");
+
         for($i = 0; $i < 10; $i++){
             $new_project = new Project();
             $new_project->name = $faker->name();
+            $new_project->type_id = $faker->optional()->randomElement($ids);
             $new_project->save();
         }
     }
